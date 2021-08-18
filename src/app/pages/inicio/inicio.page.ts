4ac1f7ee96c1ad9914c513dd800a170daf696d6d
Angel Rivilla <angelrivillaarre@gmail.com>
2021-08-18 16:52:39 +0200

    commit

---
+++ src/app/pages/inicio/inicio.page.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Componente } from 'src/app/interfaces/componente';
+
+
+@Component({
+  selector: 'app-inicio',
+  templateUrl: './inicio.page.html',
+  styleUrls: ['./inicio.page.scss'],
+})
+export class InicioPage implements OnInit {
+
+  componentes: Componente[] = [
+    {
+      icon: 'american-football',
+      name: 'Action Sheet',
+      redirectTo: '/action-sheet'
+    },
+    {
+      icon: 'logo-apple-appstore',
+      name: 'Alert',
+      redirectTo: '/alert'
+    }
+  ];
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
